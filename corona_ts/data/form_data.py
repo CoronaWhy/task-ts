db6b3c44cb4eed73dfaa8bf125467843069df79d
Isaac Godfried <igodfried@isaac26.com>
2020-04-30 21:22:39 -0400

    add files for production and base dirs

---
+++ corona_ts/data/form_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def loop_through_geo_codes(df:pd.DataFrame, column:str='full_county', cut_threshold=60):
+    """
+    Function to split data-frame based on state or county.
+    """
+    df_county_list = []
+    df['full_county'] = df['state'] + "_" + df['county'] 
+    for code in df['full_county'].unique():
+        mask = df['full_county'] == code
+        df_code = df[mask]
+        ts_count = len(df_code)
+        if ts_count > cut_threshold:
+            df_county_list.append(df_code)
+    return df_county_list 
+
