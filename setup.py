5448e2a244c1c0804cb78020b3fec9690df8fb60
isaacmg <igodfried@isaac26.com>
2020-05-14 15:52:35 -0400

    fix setup py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setuptools.setup(
     name="corona-ts-tools", # Replace with your own username
     version="0.0.1",
     author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author_email="igodfried@isaac26.com",
+    description="A package for forecasting CoronaVirus infection rates",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
