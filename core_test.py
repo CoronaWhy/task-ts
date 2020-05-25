e14269c0744cdb4303ff52a2164a0bd4dfcafdb0
isaacmg <igodfried@isaac26.com>
2020-05-25 12:41:35 -0400

    fixing df test

--- core_test.py
+++ core_test.py
@@ -9,9 +9,14 @@ class TestCoreMethods(unittest.TestCase):
     def test_region(self):
         df = load_data()
         df_ny = region_df_format(df, 'New York_New York County')
-        self.assertTrue(df_ny)
+        self.assertTrue(df_ny.any())
         self.assertEqual(df_ny.iloc[0]['new_cases'], 0)
         self.assertEqual(df_ny.iloc[0]['deaths'], 0)
     
+    def test_loop_through(self):
+        df = load_data()
+        df_list = loop_through_locations(df, 90)
+        self.assertGreater(len(df_list), 50)
+        
 if __name__ == '__main__':
     unittest.main()
