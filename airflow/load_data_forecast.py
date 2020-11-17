ae5fc13edb4fbcf3268081a17f60c604e92dc659
Kevin Li <kevin.li.wa@gmail.com>
2020-11-17 00:03:47 -0800

    Fixing the broken code for the dag

--- airflow/load_data_forecast.py
+++ airflow/load_data_forecast.py
@@ -1,3 +1,4 @@
+import datetime
 from datetime import timedelta
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
@@ -10,7 +11,7 @@ from airflow import DAG, settings
 # Connects to GoogleCloud
 from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
 # These args will get passed on to each operator
-import json, os
+import json, os, datetime
 import pandas as pd
 import numpy as np
 # Needed for load_data
@@ -49,34 +50,11 @@ def data_to_GCS(csv_name: str, folder_name: str,
     data = load_data()
     df = pd.DataFrame(data=data)
     df.to_csv('corona_data.csv', index=False)
-    columns_to_consider_for_uniqueness=['country', 'region', 'sub_region']
-    unique_column_name='full_county'
-    minimum_datapoints_threshold=60
-    """
-    Function to split data-frame based on state or county.
-    """
-    unique_df_list = []
-    for col in columns_to_consider_for_uniqueness:
-        df[col] = df[col].fillna('').apply(lambda x: x.replace(" ", "_"))
-    df[unique_column_name] = df[columns_to_consider_for_uniqueness[0]].str.cat(df[columns_to_consider_for_uniqueness[1:]], sep="_>
-    for i, g in df.groupby('full_county'):
-        df_code = g.copy()
-        ts_count = len(df_code)
-        if ts_count > minimum_datapoints_threshold:
-            df_code.reset_index(drop = True).loc[:, ~df.columns.str.contains('^Unnamed')].to_csv('{}.csv'.format(i), index = Fals>
-            hook.upload(bucket_name,
-                    object='{}/{}.csv'.format(folder_name, i),
-                    filename='{}.csv'.format(i),
-                    mime_type='text/csv')
-    """ Function for full data pull
-    df.to_csv('corona_data.csv', index=False)
-    hook.upload(bucket_name,
-                object='{}/{}.csv'.format(folder_name, csv_name),
-                filename='corona_data.csv',
+
+    hook.upload(bucket_name, 
+                object='{}/{}.csv'.format(folder_name, csv_name), 
+                filename='corona_data.csv', 
                 mime_type='text/csv')
-    """
-
-
 
 default_args = {
     'owner': 'airflow',
